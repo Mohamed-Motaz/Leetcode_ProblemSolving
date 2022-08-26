40fd1cd87f1198a093ffc46a0b3624b7149579fd
Mohamed Motaz <53558209+Mohamed-Motaz@users.noreply.github.com>
2022-08-26 15:56:46 +0200

    Time: 10 ms (73.23%), Space: 7.2 MB (74.63%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.cpp
+++ 567-permutation-in-string/567-permutation-in-string.cpp
@@ -1,44 +1,35 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        vector<int> v(26, 0), t(26, 0);
-        for (auto e: s1) v[e - 'a']++;
+        int m = s1.size(), n = s2.size();
+        if (m > n)
+            return false;
+        
+        vector<int> v(26, 0);
+        
+        for (int i = 0; i < m; i++){
+            v[s1[i] - 'a']--;
+            v[s2[i] - 'a']++;
+        }
+        
+        if (isPermu(v))
+            return true;
 
-        int l = 0, r = 0;
-        while (l <= r && r < s2.size()){
-            t[s2[r] - 'a']++;
-
-            if (check(v, t)) {
-                if (done(v, t))
-                    return true;
-                else {
-                    r++;
-                    continue;
-                }
-            }
-            else{
-                t[s2[l] - 'a']--;
-                l++; r++;
-            }
-
+        for (int i = m; i < n; i++){
+            v[s2[i] - 'a']++;
+            v[s2[i - m] - 'a']--;
+            if (isPermu(v))
+                return true;
         }
 
         return false;
     }
     
-    bool check(vector<int> &v, vector<int>& t){
-        for (int i = 0; i < v.size(); i++){
-            if (t[i] > v[i])
+    bool isPermu(vector<int>& v){
+        for (auto e: v)
+            if (e != 0)
                 return false;
-        }
         return true;
     }
 
-    bool done(vector<int> &v, vector<int>& t){
-        for (int i = 0; i < v.size(); i++){
-            if (t[i] != v[i])
-                return false;
-        }
-        return true;
-    }
 };

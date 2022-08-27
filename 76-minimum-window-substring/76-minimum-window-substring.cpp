55d15bf1f865b8af1b5ef9fdf540906ebfa7ceec
Mohamed Motaz <53558209+Mohamed-Motaz@users.noreply.github.com>
2022-08-27 17:38:06 +0200

    Time: 19 ms (76.81%), Space: 8 MB (41.51%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.cpp
+++ 76-minimum-window-substring/76-minimum-window-substring.cpp
@@ -1,35 +1,36 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        deque<char> res, tmp;
-        unordered_map<char, int> m1, m2;
-        for (auto e: t) m2[e]++;
-        int need = m2.size(), have = 0;
-        int l = 0, r = 0;
+        int mnLen = INT32_MAX;
+    int start = -1;
+    unordered_map<char, int> m1, m2;
+    for (auto e: t) m2[e]++;
+    int need = m2.size(), have = 0;
+    int l = 0, r = 0;
 
-        while (r < s.size()){
-            char cur = s[r];
-            m1[cur]++;
-            tmp.push_back(cur);
-            if (m1[cur] == m2[cur])
-                have++;
+    while (r < s.size()){
+        char cur = s[r];
+        m1[cur]++;
+        if (m1[cur] == m2[cur])
+            have++;
 
-            while (need == have){
-                if (res.empty())
-                    res = tmp;
-                else if (tmp.size() < res.size())
-                    res = tmp;
-                char remove = s[l];
-                if (m1[remove] == m2[remove])
-                    have--;
-                tmp.pop_front();
-                m1[remove]--;
-                l++;
+        while (need == have){
+            if  (r - l + 1 < mnLen){
+                mnLen = r - l + 1;
+                start = l;
             }
-            r++;
+
+            char remove = s[l];
+            if (m1[remove] == m2[remove])
+                have--;
+            m1[remove]--;
+            l++;
         }
-        string ans = "";
-        while (res.size()) ans += res.front(), res.pop_front();
-        return ans;
+        r++;
+    }
+    if (start == -1)
+        return "";
+    else
+        return s.substr(start, mnLen);
     }
 };

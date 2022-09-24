14c2a8956f42e8abce3c742c45a001168bfb07d7
Mohamed Motaz <53558209+Mohamed-Motaz@users.noreply.github.com>
2022-09-24 23:08:14 +0200

    Time: 76 ms (87.59%), Space: 48.5 MB (24.71%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -1,31 +1,16 @@
 class Solution {
 public:
-    int dp[10004];
-    vector<int> v;
-
-    bool solve(int idx){
-        if (idx == v.size() - 1)
-            return true;
-        if (idx >= v.size())
-            return false;
-
-        int &ans = dp[idx];
-        if (ans != -1)
-            return ans;
-
-        ans = 0;
-
-        for (int i = idx; i < v.size() && i <= idx + v[idx]; i++){
-            ans += solve(i);
-            if (ans)
-                return ans = true;
-        }
-        return ans = false;
-    }
 
     bool canJump(vector<int>& nums) {
-        memset(dp, -1, sizeof dp);
-        v = nums;
-        return solve(0);
+        int mx = nums[0];
+
+        for (int i = 0; i <= mx && i < nums.size(); i++){
+            if (i == nums.size() - 1)
+                return true;
+            mx = max(mx, i + nums[i]);
+        }
+
+        return false;
+        
     }
 };

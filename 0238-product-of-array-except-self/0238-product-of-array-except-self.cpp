805fc54f26fc1be225da261cba488d2fb83788b0
Mohamed Motaz <53558209+Mohamed-Motaz@users.noreply.github.com>
2023-01-28 01:26:36 +0200

    Time: 20 ms (94.47%), Space: 23.9 MB (89.55%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -3,22 +3,19 @@ public:
     vector<int> productExceptSelf(vector<int>& nums) {
         vector<int> sol(nums.size());
 
-        int tmp = nums[0];
-        for (int i = 1; i < nums.size(); i++)
-        {
-            sol[i] = tmp;
-            tmp *= nums[i];
-        }
+    int tmp = 1;
+    for (int i = 0; i < nums.size(); i++)
+    {
+        sol[i] = tmp;
+        tmp *= nums[i];
+    }
 
-        tmp = nums.back();
-        for (int i = nums.size() - 2; i >= 0; i--)
-        {
-            if (!i)
-                sol[i] = tmp;
-            else
-                sol[i] *= tmp;
-            tmp *= nums[i];
-        }
-        return sol;
+    tmp = 1;
+    for (int i = nums.size() - 1; i >= 0; i--)
+    {
+        sol[i] *= tmp;
+        tmp *= nums[i];
+    }
+    return sol;
     }
 };

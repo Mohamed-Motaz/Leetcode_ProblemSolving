c5b0443b51bec792654e8b190afc0d9d9b7e0cda
Mohamed Motaz <53558209+Mohamed-Motaz@users.noreply.github.com>
2023-02-03 19:55:19 +0200

    Time: 30 ms (90.88%), Space: 71.5 MB (53.89%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -11,45 +11,23 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        vector<int> v1, v2;
-        while (l1 != nullptr)
-            v1.push_back(l1->val), l1 = l1->next;
-        while (l2 != nullptr)
-            v2.push_back(l2->val), l2 = l2->next;
-
-        if (v1.size() > v2.size())
-            swap(v1, v2);
-
         int carry = 0;
-
-        vector<int> res;
-        for (int i = 0; i < v1.size(); i++)
+        ListNode *head = new ListNode(-1);
+        ListNode *res = head;
+        while (l1 || l2 || carry)
         {
-            int sum = v1[i] + v2[i] + carry;
-            res.push_back(sum % 10);
-            sum /= 10;
-            carry = sum;
-        }
+            int f = 0, s = 0;
+            if (l1)
+                f = l1->val, l1 = l1->next;
+            if (l2)
+                s = l2->val, l2 = l2->next;
 
-        for (int i = v1.size(); i < v2.size(); i++)
-        {
-            int sum = carry + v2[i];
-            res.push_back(sum % 10);
-            sum /= 10;
-            carry = sum;
+            int sum = f + s + carry;
+            head->next = new ListNode(sum % 10);
+            head = head->next;
+            carry = sum / 10;
         }
-        if (carry)
-            res.push_back(carry);
-
-        ListNode *dummy = new ListNode(-1);
-        ListNode *copy = dummy;
-
-        for (int i = 0; i < res.size(); i++)
-        {
-            dummy->next = new ListNode(res[i]);
-            dummy = dummy->next;
-        }
-        return copy->next;
+        return res->next;
         
     }
 };

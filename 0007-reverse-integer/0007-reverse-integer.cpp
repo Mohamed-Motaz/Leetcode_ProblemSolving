1cebcf8d0f37b0b4a0ae47b066166efb43eb6575
Mohamed Motaz <53558209+Mohamed-Motaz@users.noreply.github.com>
2023-02-06 21:42:05 +0200

    Time: 0 ms (100.00%), Space: 6 MB (64.01%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -1,23 +1,17 @@
 class Solution {
 public:
     int reverse(int x) {
-        bool neg = false;
-        if (x < 0)
-            neg = true;
-
+        bool neg = x < 0;
         x = abs(x);
-        string s = to_string(x);
+        long long ans = 0;
 
-        long long tmp = 0;
-        long long times = 1;
-        for (int j = 0; j < s.size(); j++)
+        while (x)
         {
-            tmp += (s[j] - '0') * times;
-            times *= 10;
+            ans *= 10;
+            ans += x % 10;
+            x /= 10;
         }
-        neg ? tmp *= -1 : tmp;
-        if (tmp > INT32_MAX || tmp < INT32_MIN)
-            return 0;
-        return tmp;
+        neg ? ans *= -1 : ans;
+        return (ans > INT32_MAX || ans < INT32_MIN) ? 0 : ans;
     }
 };

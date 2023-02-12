5e9bcd14551338099701ef5769d95558a3b9ce06
Mohamed Motaz <53558209+Mohamed-Motaz@users.noreply.github.com>
2023-02-12 22:04:11 +0200

    Time: 3 ms (60.52%), Space: 5.9 MB (76.29%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.cpp
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -2,15 +2,11 @@ class Solution {
 public:
     uint32_t reverseBits(uint32_t n) {
         uint32_t ans = 0;
-        int p = 32;
-        while (n){
-            int val = n % 2;
-            n /= 2;
-            p--;
-            if (!val){
-                continue;
-            }
-            ans += pow(2, p);
+
+        for (int i = 0; i < 32; i++){
+            ans <<= 1;
+            ans |= n & 1;
+            n >>= 1;
         }
         return ans;
     }

9b3769896783c63a1d965c94d443d9d370bff0b0
Mohamed Motaz <53558209+Mohamed-Motaz@users.noreply.github.com>
2023-02-21 16:13:54 +0200

    Time: 8 ms (72.32%), Space: 8.8 MB (75.50%) - LeetHub

---
+++ 0684-redundant-connection/0684-redundant-connection.cpp
@@ -0,0 +1,56 @@
+int parent[1001];
+int sz[1001];
+
+void make_set(int v)
+{
+    parent[v] = v;
+    sz[v] = 1;
+}
+
+int find_set(int v)
+{
+    if (v == parent[v])
+        return v;
+    return parent[v] = find_set(parent[v]);
+}
+
+
+void union_sets(int a, int b)
+{
+    a = find_set(a);
+    b = find_set(b);
+    if (a != b)
+    {
+        if (sz[a] < sz[b])
+            swap(a, b);
+        parent[b] = a;
+        sz[a] += sz[b];
+    }
+}
+
+
+class Solution {
+public:
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        for (int i = 1; i <= edges.size(); i++)
+        make_set(i);
+    
+        vector<int> sol;
+        for (int i = 0; i < edges.size(); i++)
+        {
+            int a = find_set(edges[i][0]);
+            int b = find_set(edges[i][1]);
+
+            if (a == b){
+                sol = {edges[i][0], edges[i][1]};
+                continue;
+            }
+            //union
+            if (sz[a] < sz[b])
+                swap(a, b);
+            parent[b] = a;
+            sz[a] += sz[b];
+        }
+        return sol;
+    }
+};

d529e1a3872e5d9139d7351a969160640762fa2a
Mohamed Motaz <53558209+Mohamed-Motaz@users.noreply.github.com>
2023-03-31 17:39:49 +0200

    Time: 123 ms (12.48%), Space: 25.9 MB (14.08%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
-    vector<vector<string>> groupAnagrams(vector<string>& strs)     {
-       unordered_map<string, vector<int>> mp;
-        for (int i = 0; i < strs.size(); i++)
-        {
-            string t = strs[i];
-            sort(t.begin(), t.end());
-            mp[t].push_back(i);
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> mp;
+        const int n = strs.size();
+        for (int i = 0; i < n; i++){
+            mp[getKey(strs[i])].push_back(strs[i]);
         }
-        vector<vector<string>> sol(mp.size());
-        int idx = 0;
-        for (pair<const string, vector<int>> &e: mp){
-            for (int i: e.second){
-                sol[idx].push_back(strs[i]);
-            }
-            ++idx;
+        vector<vector<string>> res;
+        for (auto it = mp.begin(); it != mp.end(); it++){
+            res.push_back(it->second);
         }
+        return res;
+    }
 
-        return sol;
+    string getKey(string &s){
+        vector<int> v(26, 0);
+        for (char c: s) v[c - 'a']++;
+        string res = "";
+        for (int n: v)res += to_string(n + '0');
+        return res;
     }
 };

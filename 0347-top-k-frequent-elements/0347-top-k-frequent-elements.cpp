911affed50b1ad145598016a477e113876d8b100
Mohamed Motaz <53558209+Mohamed-Motaz@users.noreply.github.com>
2023-04-01 20:18:37 +0200

    Time: 17 ms (60.80%), Space: 15.4 MB (8.23%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        unordered_map<int, int> ctr;
-        for (int &e: nums){
-            ctr[e]++;
+        unordered_map<int, int> mp;
+        for (int &i : nums)
+            mp[i]++;
+
+        vector<vector<int>> buckets(nums.size() + 1);
+        for (auto it = mp.begin(); it != mp.end(); it++)
+        {
+            buckets[it->second].push_back(it->first);
         }
 
-        vector<vector<int>> v(nums.size() + 1);
-        for (auto e: ctr){
-            v[e.second].push_back(e.first);
+        vector<int> res;
+        for (int i = buckets.size() - 1; i >= 0; i--)
+        {
+            for (int j = 0; k && j < buckets[i].size(); j++)
+            {
+                res.push_back(buckets[i][j]);
+                k--;
+            }
         }
-
-        vector<int> sol;
-        for (int i = v.size() - 1; i >= 0 && k; i--){
-            if (v[i].size())
-                for (int j = 0; j < v[i].size() && k; j++)
-                    sol.push_back(v[i][j]), k--;
-        }
-
-        return sol;
+        return res;
     }
 };

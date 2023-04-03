8170be8da60d5f8823e9239428c7d3b348cc6b5e
Mohamed Motaz <53558209+Mohamed-Motaz@users.noreply.github.com>
2023-04-03 02:57:00 +0200

    Time: 72 ms (51.78%), Space: 48.3 MB (81.39%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,13 +1,10 @@
 class Solution {
 public:
-
     bool canJump(vector<int>& nums) {
-      int mx = 0;
-        for (int i = 0; i < nums.size() && i <= mx; i++)
-        {
-            mx = max(mx, i + nums[i]);
+        int mx = nums[0];
+        for (int i = 0; i <= mx && i < nums.size(); i++){
+            mx = max(mx, nums[i] + i);
         }
         return mx >= nums.size() - 1;
-        
     }
 };

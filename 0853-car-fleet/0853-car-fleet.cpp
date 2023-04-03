03676f9e7791890749025c2a88f35b426971ae13
Mohamed Motaz <53558209+Mohamed-Motaz@users.noreply.github.com>
2023-04-03 21:34:58 +0200

    Time: 203 ms (79.80%), Space: 76.9 MB (85.47%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.cpp
+++ 0853-car-fleet/0853-car-fleet.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
     int carFleet(int target, vector<int>& position, vector<int>& speed) {
-        int ctr = 0;
-        int n = position.size();
-        vector<pair<double, double>> v(n);
-        for (int i = 0; i < n; i++)
-            v[i] = make_pair(position[i], speed[i]);
+        vector<pair<int, double>> v(position.size());
+        for (int i = 0; i < position.size(); i++) 
+            v[i] = make_pair(position[i], (target - position[i]) / (double)speed[i]);
+
         sort(v.begin(), v.end());
 
+        int ctr = 0;
         double mxTime = 0;
-        for (int i = n - 1; i >= 0; i--)
-        {
-            double curTime = (target - v[i].first) / v[i].second;
-            if (curTime > mxTime)
-            {
-                mxTime = curTime;
+        for (int i = v.size() - 1; i >= 0; i--){
+            if (v[i].second > mxTime){
                 ctr++;
             }
+            mxTime = max(mxTime, v[i].second);
         }
+
         return ctr;
     }
 };
+
+
+// s = d / t   t = d / s

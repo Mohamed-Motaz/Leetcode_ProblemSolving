17c7140cfb2f912c80355af61fae5585f354b04b
Mohamed Motaz <53558209+Mohamed-Motaz@users.noreply.github.com>
2023-04-16 16:14:32 +0200

    Time: 3 ms (55.99%), Space: 5.9 MB (64.88%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -4,14 +4,12 @@ public:
         bool neg = x < 0;
         x = abs(x);
         long long ans = 0;
-
-        while (x)
-        {
+        while (x){
             ans *= 10;
             ans += x % 10;
             x /= 10;
         }
-        neg ? ans *= -1 : ans;
-        return (ans > INT32_MAX || ans < INT32_MIN) ? 0 : ans;
+        if (neg) ans *= -1;
+        return ans <= INT32_MAX && ans >= INT32_MIN ? ans : 0;
     }
 };

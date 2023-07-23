ef613985c31a8c998c17d3048ba963772c40f82d
Mohamed Motaz <53558209+Mohamed-Motaz@users.noreply.github.com>
2023-07-23 23:10:51 +0300

    Time: 55 ms (30.97%), Space: 13.9 MB (71.05%) - LeetHub

--- 518-coin-change-ii/518-coin-change-ii.cpp
+++ 518-coin-change-ii/518-coin-change-ii.cpp
@@ -1,38 +1,39 @@
 class Solution {
 public:
-    int dp[300][5001];
-    vector<int> v;
-    int target;
-    int solve(int idx, int curAmount)
+    int dp[301][5001];
+vector<int> v;
+
+int solve(int idx, int sum, int target)
+{
+    if (idx == v.size())
     {
-        if (idx >= v.size())
-        {
-            return (curAmount == target);
-        }
+        return sum == target;
+    }
 
-        int &ans = dp[idx][curAmount];
-        if (ans != -1)
-            return ans;
-        ans = 0;
-
-        // don't take the cur val
-        ans += solve(idx + 1, curAmount);
-
-        while (curAmount + v[idx] <= target)
-        {
-            curAmount += v[idx];
-            ans += solve(idx + 1, curAmount);
-        }
-
+    int &ans = dp[idx][sum];
+    if (ans != -1)
         return ans;
-    }
 
-    int change(int amount, vector<int> &coins)
+    ans = 0;
+    if (sum + v[idx] <= target)
     {
-        target = amount;
-        v = coins;
-        memset(dp, -1, sizeof dp);
-        return solve(0, 0);
+        // take
+        ans += solve(idx, sum + v[idx], target);
+        // leave
+        ans += solve(idx + 1, sum, target);
     }
+    else
+    {
+        // have to leave
+        ans += solve(idx + 1, sum, target);
+    }
+    return ans;
+}
 
+int change(int amount, vector<int> &coins)
+{
+    memset(dp, -1, sizeof dp);
+    v = coins;
+    return solve(0, 0, amount);
+}
 };

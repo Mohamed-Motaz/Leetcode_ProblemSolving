7ec5839bed14ad95c3472a35f853607ab5901615
Mohamed Motaz <53558209+Mohamed-Motaz@users.noreply.github.com>
2023-07-24 00:18:28 +0300

    Time: 2 ms (55.80%), Space: 6.1 MB (92.36%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -26,8 +26,16 @@ public:
 
     int numDecodings(string s)
     {
-        memset(dp, -1, sizeof dp);
-        ss = s;
-        return solve(0);
+        dp[s.size()] = 1;
+        for (int i = s.size() - 1; i >= 0; i--){
+            if (s[i] == '0') dp[i] = 0;
+            else{
+                dp[i] += dp[i + 1];
+                if (i + 1 < s.size() && ((s[i] == '1' || (s[i] == '2' && s[i + 1] <= '6')))){
+                    dp[i] += dp[i + 2];
+                }
+            }
+        }
+        return dp[0];
     }
 };

241d9777c83f3bc016618044a12bfefb4049224c
Mohamed Motaz <53558209+Mohamed-Motaz@users.noreply.github.com>
2023-08-10 23:33:34 +0300

    Time: 0 ms (100.00%), Space: 7 MB (22.32%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -1,35 +1,39 @@
 class Solution {
 public:
-    vector<vector<bool>> vis;
-    vector<int> res;
-    bool ok(int newX, int newY, vector<vector<int>>& matrix){
-        return newX >= 0 && newX < matrix.size() && 
-            newY >= 0 && newY < matrix[0].size() && 
-            !vis[newX][newY];
-    }
-    
-    void dfs(int x, int y, int prev, vector<vector<int>>& matrix){
-        if (!ok(x, y, matrix))    
-            return;
-        vis[x][y] = 1;
-        res.push_back(matrix[x][y]);
-        
-        //prioritize same path
-        if (prev == 0) dfs(x, y + 1, prev, matrix);
-        if (prev == 1) dfs(x + 1, y, prev, matrix);
-        if (prev == 2) dfs(x, y - 1, prev, matrix);
-        if (prev == 3) dfs(x - 1, y, prev, matrix);
-        
-        //else just keep going if can't go to the same path
-        dfs(x, y + 1, (prev + 1) % 4, matrix);
-        dfs(x + 1, y, (prev + 1) % 4, matrix);
-        dfs(x, y - 1, (prev + 1) % 4, matrix);
-        dfs(x - 1, y, (prev + 1) % 4, matrix);
-    }
     
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
-        vis.resize(matrix.size(), vector<bool>(matrix[0].size()));
-        dfs(0, 0, 0, matrix);
+        const int rows = matrix.size(), cols = matrix[0].size();
+        int top = 0, left = 0, right = cols - 1, bottom = rows - 1;
+        vector<int> res;
+        
+        while (top <= bottom && left <= right){
+            for (int i = left; i <= right; i++){
+                res.push_back(matrix[top][i]);
+            }    
+            ++top;
+            
+            for (int i = top; i <= bottom; i++){
+                res.push_back(matrix[i][right]);
+            }
+            --right;
+            
+            if (top > bottom)
+                break;
+            
+            for (int i = right; i >= left; i--){
+                res.push_back(matrix[bottom][i]);
+            }
+            --bottom;
+            
+            if (left > right)
+                break;
+            
+            for (int i = bottom; i >= top; i--){
+                res.push_back(matrix[i][left]);
+            }
+            ++left;
+        }
+        
         return res;
     }
 };

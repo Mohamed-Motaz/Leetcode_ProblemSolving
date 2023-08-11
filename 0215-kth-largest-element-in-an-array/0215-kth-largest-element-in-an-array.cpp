a74a895929e14eb713b9787d866947d17703dec7
Mohamed Motaz <53558209+Mohamed-Motaz@users.noreply.github.com>
2023-08-12 01:02:18 +0300

    Time: 168 ms (9.30%), Space: 45.4 MB (86.22%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.cpp
@@ -1,16 +1,33 @@
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        priority_queue<int, vector<int>, greater<int>> pq;
-        for (int i = 0; i < nums.size(); i++){
-            if (pq.size() < k) {
-                pq.push(nums[i]);
-            }else if (nums[i] > pq.top()){
-                pq.pop();
-                pq.push(nums[i]);
+        k = nums.size() - k;
+        return quickSelect(0, nums.size() - 1, k, nums);
+    }
+    
+    int quickSelect(int s, int e, int k, vector<int> &nums){
+        if (e < s)
+            return 0;
+        
+        int pivot = partition(s, e, nums);
+        
+        if (pivot < k) return quickSelect(pivot + 1, e, k, nums); //check the right
+        if (pivot > k) return quickSelect(s, pivot - 1, k, nums); //check the left
+        return nums[pivot]; //found it!
+
+    }
+    
+    int partition(int s, int e, vector<int> &nums){
+        int pivot = nums[e];
+        int left = s;
+        
+        for (int i = s; i <= e - 1; i++){
+            if (nums[i] <= pivot){
+                swap(nums[i], nums[left++]);
             }
         }
         
-        return pq.top();
+        swap(nums[e], nums[left]);
+        return left;
     }
 };

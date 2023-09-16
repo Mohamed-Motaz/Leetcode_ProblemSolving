77fc925328c62c57a351f03baca3e487c746c19b
Mohamed Motaz <53558209+Mohamed-Motaz@users.noreply.github.com>
2023-09-16 21:51:12 +0300

    Time: 10 ms (87.18%), Space: 11.7 MB (54.29%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    
+    pair<int, int> solve(int left, int right, string &s){
+        int sz = 0;
+        while (left >= 0 && right < s.size() && s[left] == s[right]){
+            if (left == right) sz++;
+            else sz+=2; 
+            left--, right++;
+        }
+        return make_pair(++left, --right);
+    }
+    
+    string longestPalindrome(string s) {
+        int mx = 1;
+        string res = string(1, s[0]);
+        
+        for (int i = 0; i < s.size(); i++){
+            pair<int, int> first = solve(i, i, s);
+            int firstSz = first.second - first.first + 1;
+            
+            pair<int, int> second = solve(i, i + 1, s);
+            int secondSz = second.second - second.first + 1;
+
+            if (firstSz > mx){
+                res = s.substr(first.first, firstSz);
+                mx = firstSz;
+            }
+            if (secondSz > mx){
+                res = s.substr(second.first, secondSz);
+                mx = secondSz;
+            }
+        }
+        return res;
+    }
+};

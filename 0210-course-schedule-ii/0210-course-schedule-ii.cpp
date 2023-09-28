64ea214d07d7c3eda1f12b274cff725988a812f9
Mohamed Motaz <53558209+Mohamed-Motaz@users.noreply.github.com>
2023-09-28 20:30:39 +0300

    Time: 14 ms (85.29%), Space: 14.5 MB (27.89%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
 
-    vector<int> bfs(int numCourses, vector<vector<int>>& prerequisites){
+    vector<int> bfs_kahn_algo(int numCourses, vector<vector<int>>& prerequisites){
         vector<vector<int>> adjList(numCourses, vector<int>());
         vector<int> inDegree(numCourses);
         for (vector<int> &v: prerequisites){
@@ -28,9 +28,35 @@ public:
         return res.size() == numCourses ? res : vector<int>();
     }
     
+    void dfs(int node, vector<int>& res, vector<int>& inDegree, vector<vector<int>> &adjList){
+        res.push_back(node);
+        inDegree[node] = -1;
+        for (int i: adjList[node]){
+            if (!(--inDegree[i]))
+                dfs(i, res, inDegree, adjList);
+        }
+        return;
+    }
+    
+    vector<int> dfs_top_sort(int numCourses, vector<vector<int>>& prerequisites){
+        vector<vector<int>> adjList(numCourses, vector<int>());
+        vector<int> inDegree(numCourses);
+        for (vector<int> &v: prerequisites){
+            adjList[v[1]].push_back(v[0]);
+            inDegree[v[0]]++;
+        }
+        
+        vector<int> res;
+        for (int i = 0; i < numCourses; i++){
+            if (!inDegree[i])
+                dfs(i, res, inDegree, adjList);
+        }
+        return res.size() == numCourses ? res : vector<int>();
+    }
+    
 
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-        return bfs(numCourses, prerequisites);
-        //return dfs(numCourses, prerequisites);
+        //return bfs_kahn_algo(numCourses, prerequisites);
+        return dfs_top_sort(numCourses, prerequisites);
     }
 };

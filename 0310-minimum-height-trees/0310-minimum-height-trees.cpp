f8adc9c8887bb71f2f699b273021e7bb596bfb77
Mohamed Motaz <53558209+Mohamed-Motaz@users.noreply.github.com>
2023-09-28 23:10:19 +0300

    Time: 198 ms (10.71%), Space: 73.3 MB (14.81%) - LeetHub

--- 0310-minimum-height-trees/0310-minimum-height-trees.cpp
+++ 0310-minimum-height-trees/0310-minimum-height-trees.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    vector<int> findMinHeightTrees(int n, vector<vector<int>>& edges) {
+    vector<int> top_sort(int n, vector<vector<int>>& edges){
         vector<vector<int>> adjList(n);
         vector<int> inDegree(n, 0);
         for (vector<int> &v: edges){
@@ -31,5 +31,44 @@ public:
         }
         if (n == 1) ans.push_back(0);
         return ans;
+    }
+    
+    vector<int> dfs(int node, vector<bool>& vis, vector<vector<int>>& adjList){
+
+        vector<int> longestPath, childPath;
+        vis[node] = 1;
+        for (int child: adjList[node]){
+            if (!vis[child]){
+                childPath = dfs(child, vis, adjList);
+                if (childPath.size() > longestPath.size())
+                    longestPath = move(childPath);
+            }
+        }
+        vis[node] = 0;
+        longestPath.push_back(node);
+        return longestPath;
+    }
+    
+    vector<int>two_dfs(int n, vector<vector<int>>& edges){
+        vector<vector<int>> adjList(n);
+        for (vector<int> &v: edges){
+            adjList[v[0]].push_back(v[1]);
+            adjList[v[1]].push_back(v[0]);
+        }
+        
+        vector<bool> vis(n);
+        vector<int> path = dfs(0, vis, adjList);
+        vis = vector<bool>(n, 0);
+        path = dfs(path[0], vis, adjList);
+        
+        if (path.size() % 2){
+            return {path[path.size() / 2]};
+        }
+        return {path[path.size() / 2 - 1], path[path.size() / 2]};
+    }
+    
+    vector<int> findMinHeightTrees(int n, vector<vector<int>>& edges) {
+        //return top_sort(n, edges);
+        return two_dfs(n, edges);
     }
 };

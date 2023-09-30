31d31e3f8838c346e6ed2741ca54a2adc22806bc
Mohamed Motaz <53558209+Mohamed-Motaz@users.noreply.github.com>
2023-09-30 12:47:20 +0300

    Time: 18 ms (53.43%), Space: 14.7 MB (25.92%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.cpp
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -28,12 +28,12 @@ public:
         return res.size() == numCourses ? res : vector<int>();
     }
     
-    void dfs(int node, vector<int>& res, vector<int>& inDegree, vector<vector<int>> &adjList){
+    void dfs_topo(int node, vector<int>& res, vector<int>& inDegree, vector<vector<int>> &adjList){
         res.push_back(node);
         inDegree[node] = -1;
         for (int i: adjList[node]){
             if (!(--inDegree[i]))
-                dfs(i, res, inDegree, adjList);
+                dfs_topo(i, res, inDegree, adjList);
         }
         return;
     }
@@ -49,14 +49,48 @@ public:
         vector<int> res;
         for (int i = 0; i < numCourses; i++){
             if (!inDegree[i])
-                dfs(i, res, inDegree, adjList);
+                dfs_topo(i, res, inDegree, adjList);
         }
         return res.size() == numCourses ? res : vector<int>();
+    }
+    
+    bool dfs(int cur, vector<vector<int>>& adjList, vector<bool> &vis, vector<bool>& curStack,
+            vector<int>& result){
+        if (curStack[cur] == 1) return 0; //found a cycle
+        if (vis[cur] == 1) return 1; //visited before, but not in cur stack
+        vis[cur] = 1;
+        curStack[cur] = 1;
+        for (int child: adjList[cur]){
+            if (!dfs(child, adjList, vis, curStack, result)) return 0;
+        }
+        
+        curStack[cur] = 0;
+        result.push_back(cur);
+        return 1;
+    }
+    
+    vector<int> dfs_sol(int numCourses, vector<vector<int>>& prerequisites){
+        //try to search for a cycle
+        vector<vector<int>> adjList(numCourses, vector<int>());
+        for (vector<int> &v: prerequisites){
+            adjList[v[1]].push_back(v[0]);
+        }
+        
+        vector<bool> vis(numCourses);
+        vector<bool> curStack(numCourses);
+        vector<int> result;
+        
+        for (int i = 0; i < numCourses; i++){
+            if (!dfs(i, adjList, vis, curStack, result)) return {};
+        }
+        reverse(result.begin(), result.end());
+        return result;
     }
     
 
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
         //return bfs_kahn_algo(numCourses, prerequisites);
-        return dfs_top_sort(numCourses, prerequisites);
+        //return dfs_top_sort(numCourses, prerequisites);
+        return dfs_sol(numCourses, prerequisites);
     }
 };

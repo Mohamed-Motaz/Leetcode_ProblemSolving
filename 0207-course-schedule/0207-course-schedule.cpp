cecabac5789e214706d2c45e4ca19422e69de1f0
Mohamed Motaz <53558209+Mohamed-Motaz@users.noreply.github.com>
2023-09-30 12:40:29 +0300

    Time: 17 ms (61.99%), Space: 14.4 MB (35.37%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -27,7 +27,37 @@ public:
         return res.size() == numCourses ? 1 : 0;
     }
     
+    bool dfs(int cur, vector<vector<int>>& adjList, vector<bool> &vis, vector<bool>& curStack){
+        if (curStack[cur] == 1) return 0; //found a cycle
+        if (vis[cur] == 1) return 1; //visited before, but not in cur stack
+        vis[cur] = 1;
+        curStack[cur] = 1;
+        for (int child: adjList[cur]){
+            if (!dfs(child, adjList, vis, curStack)) return 0;
+        }
+        
+        curStack[cur] = 0;
+        return 1;
+    }
+    
+    bool dfs_sol(int numCourses, vector<vector<int>>& prerequisites){
+        //try to search for a cycle
+        vector<vector<int>> adjList(numCourses, vector<int>());
+        for (vector<int> &v: prerequisites){
+            adjList[v[1]].push_back(v[0]);
+        }
+        
+        vector<bool> vis(numCourses);
+        vector<bool> curStack(numCourses);
+        
+        for (int i = 0; i < numCourses; i++){
+            if (!dfs(i, adjList, vis, curStack)) return 0;
+        }
+        return 1;
+    }
+    
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        return bfs_kahn_algo(numCourses, prerequisites);
+        // return bfs_kahn_algo(numCourses, prerequisites);
+        return dfs_sol(numCourses, prerequisites);
     }
 };

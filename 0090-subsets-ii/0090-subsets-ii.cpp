fa9e98fcd5280e790d5a575ee4e162b62bf65211
Mohamed Motaz <53558209+Mohamed-Motaz@users.noreply.github.com>
2023-10-14 22:03:30 +0300

    Time: 0 ms (100.00%), Space: 7.9 MB (59.88%) - LeetHub

---
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> sol;
+        vector<int> cur;
+        dfs(0, nums, cur, sol);
+        return sol;
+    }
+    void dfs(int idx, vector<int>& nums, vector<int> &cur, vector<vector<int>>& sol){
+        if (idx >= nums.size()){
+            sol.push_back(cur);
+            return;
+        }
+        
+        
+        cur.push_back(nums[idx]);
+        dfs(idx + 1, nums, cur, sol);
+        cur.pop_back();
+        
+        idx++;
+        while (idx < nums.size() && nums[idx] == nums[idx - 1]){
+            idx++;
+        }
+        dfs(idx, nums, cur, sol);
+    }
+};

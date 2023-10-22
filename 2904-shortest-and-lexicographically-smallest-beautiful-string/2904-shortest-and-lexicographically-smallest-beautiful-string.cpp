2dc12b81929edfd0ea2d110e0fa5e501c660a132
Mohamed Motaz <53558209+Mohamed-Motaz@users.noreply.github.com>
2023-10-23 01:38:50 +0300

    Time: 2 ms (76.84%), Space: 7 MB (53.22%) - LeetHub

--- 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.cpp
+++ 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.cpp
@@ -1,15 +1,6 @@
 class Solution {
 public:
-    bool isSmaller(string s, string t){
-        if (s.size() < t.size()) return 1;
-        int i = 0;
-        while (i < s.size()){
-            if (s[i] > t[i]) return 0;
-            if (s[i] < t[i]) return 1;
-            i++;
-        }
-        return 0;
-    }
+
     string shortestBeautifulSubstring(string s, int k) {
         string res = s + s;
         for (int i = 0; i < s.size(); i++){
@@ -18,12 +9,10 @@ public:
                 if (s[j] == '1') ctr++;
                 if (ctr == k){
                     string tmp = s.substr(i, j - i + 1);
-                    if (tmp.size() <= res.size()) {
-                        if (isSmaller(tmp, res))
-                        {
-                            res = tmp;
-                        }
-                    }
+                    if (tmp.size() < res.size()) {
+                       res = tmp;
+                    }else if (tmp.size() == res.size() && tmp < res)
+                        res = tmp;
                 }
             }
         }

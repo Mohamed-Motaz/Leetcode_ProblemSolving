9d0322669f393ad76a29649f4eadda46c910625c
Mohamed Motaz <53558209+Mohamed-Motaz@users.noreply.github.com>
2023-10-23 20:40:10 +0300

    Time: 0 ms (100.00%), Space: 6.6 MB (82.67%) - LeetHub

--- 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.cpp
+++ 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.cpp
@@ -1,21 +1,33 @@
 class Solution {
 public:
-
     string shortestBeautifulSubstring(string s, int k) {
-        string res = s + s;
-        for (int i = 0; i < s.size(); i++){
-            int ctr = 0;
-            for (int j = i; j < s.size(); j++){
-                if (s[j] == '1') ctr++;
-                if (ctr == k){
-                    string tmp = s.substr(i, j - i + 1);
-                    if (tmp.size() < res.size()) {
-                       res = tmp;
-                    }else if (tmp.size() == res.size() && tmp < res)
-                        res = tmp;
+        string ans = "";
+        int ansSize = INT32_MAX;
+        int start = 0, end = 0;
+        int ctr = 0;
+        while (end < s.size()){
+            if (s[end] == '1'){
+                ctr++;
+            }
+            while (s[start] == '0' || ctr > k){
+                if (s[start] == '1') ctr--;
+                start++;
+            }
+            if (ctr == k){
+                int sz = end - start + 1;
+                string tmp = s.substr(start, sz);
+                if (sz < ansSize){
+                    ansSize = sz;
+                    ans = tmp;
+                }else if (sz == ansSize){
+                    if (tmp < ans){
+                        ans = tmp;
+                    }
                 }
             }
+            
+            end++;
         }
-        return res == s + s ? "" : res;
+        return ans;
     }
 };

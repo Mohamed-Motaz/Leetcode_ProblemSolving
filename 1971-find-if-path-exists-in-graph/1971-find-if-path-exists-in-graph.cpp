bb3fea6a94da605503fc9f630d6f9beec676cde1
Mohamed Motaz <53558209+Mohamed-Motaz@users.noreply.github.com>
2023-10-29 22:34:53 +0200

    Time: 510 ms (58.67%), Space: 166.7 MB (73.21%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -31,11 +31,71 @@ public:
         return 0;
     }
     
+    vector<int> parents;
+    vector<int> sz;
+    
+    void initUnionFind(int n){
+        parents.resize(n);
+        sz.resize(n);
+        
+        for (int i = 0; i < n;i++){
+            parents[i] = i;
+            sz[i] = 1;
+        }
+    }
+    
+    int find(int p){
+        int root = p;
+        while (parents[root] != root){
+            root = parents[root];
+        }
+        
+        //path compression
+        while (p != parents[p]){
+            int next = parents[p];
+            parents[p] = root;
+            p = next;
+        }
+        return root;
+    }
+    void unionify(int p, int q){
+        
+        int pp = find(p);
+        int qq = find(q);
+        if (pp == qq) return;
+        
+        if (sz[pp] > sz[qq]){
+            sz[pp] += sz[qq];
+            parents[qq] = pp;
+        }else{
+            sz[qq] += sz[pp];
+            parents[pp] = qq;
+        }
+        
+    }
+    bool connected(int p, int q){
+        return find(p) == find(q);
+    }
+    
+    bool solveUnionFind(int n, vector<vector<int>>& adjList, int source, int destination){
+        initUnionFind(n);
+        for (int i = 0; i < adjList.size(); i++){
+            for (int child: adjList[i]){
+                unionify(i, child);
+            }
+        }
+        return connected(source, destination);
+    }
+    
     bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
-        vis.resize(n);
+        // vis.resize(n);
+        // adjList.resize(n, vector<int>());
+        // for (auto &i: edges) adjList[i[0]].push_back(i[1]), adjList[i[1]].push_back(i[0]);
+        // //return dfs(source, adjList, destination);
+        // return bfs(source, adjList, destination);
+        
         adjList.resize(n, vector<int>());
         for (auto &i: edges) adjList[i[0]].push_back(i[1]), adjList[i[1]].push_back(i[0]);
-        //return dfs(source, adjList, destination);
-        return bfs(source, adjList, destination);
+        return solveUnionFind(n, adjList, source, destination);
     }
 };

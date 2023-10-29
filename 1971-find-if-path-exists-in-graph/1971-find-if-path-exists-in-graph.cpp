a88846f0c48d72281d10144d68dade810077caf6
Mohamed Motaz <53558209+Mohamed-Motaz@users.noreply.github.com>
2023-10-29 21:38:32 +0200

    Time: 543 ms (50.51%), Space: 239.2 MB (24.91%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.cpp
@@ -3,15 +3,24 @@ public:
     
     vector<bool> vis;
     vector<vector<int>> adjList;
-    bool dfs(int cur, int par, vector<vector<int>>& adjList,  int destination) {
+    bool dfs(int cur, vector<vector<int>>& adjList,  int destination) {
         if (cur == destination) return 1;
         if (vis[cur]) return 0;
         vis[cur] = 1;
         for (int child: adjList[cur]){
-            if (child == par)
-                continue;
-            if (dfs(child, cur, adjList, destination))
+            if (dfs(child, adjList, destination))
                 return 1;
+        }
+        return 0;
+    }
+    
+    bool bfs(int cur, int par, vector<vector<int>>& adjList, int destination){
+        queue<int> q;
+        q.push(cur);
+        vis[cur] = 1;
+        
+        while (q.size()){
+                
         }
         return 0;
     }
@@ -20,6 +29,7 @@ public:
         vis.resize(n);
         adjList.resize(n, vector<int>());
         for (auto &i: edges) adjList[i[0]].push_back(i[1]), adjList[i[1]].push_back(i[0]);
-        return dfs(source, -1, adjList, destination);
+        return dfs(source, adjList, destination);
+        return bfs(source, -1, adjList, destination);
     }
 };

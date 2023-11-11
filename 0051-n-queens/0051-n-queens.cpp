7e291c7a819d7a0c9d4b1b9a7238d7dc69b73e25
Mohamed Motaz <53558209+Mohamed-Motaz@users.noreply.github.com>
2023-11-11 11:36:59 +0200

    Time: 12 ms (27.45%), Space: 9.5 MB (24.38%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    
+    unordered_set<int> posDiagonal;
+    unordered_set<int> negDiagonal;
+    unordered_set<int> cols;
+    vector<vector<string>> sol;
+    
+    
+    
+    void solve(int row, int n, vector<string>& board){
+        if (n == row){
+            sol.push_back(board);
+            return;
+        }
+        
+        for (int col = 0; col < n; col++){
+            int pos = row + col;
+            int neg = row - col;
+            if (cols.find(col) != cols.end() ||
+                posDiagonal.find(pos) != posDiagonal.end() ||
+               negDiagonal.find(neg) != negDiagonal.end()){
+                continue;
+            }
+            cols.insert(col);
+            posDiagonal.insert(pos);
+            negDiagonal.insert(neg);
+            board[row][col] = 'Q';
+            
+            solve(row + 1, n, board);
+            
+            cols.erase(col);
+            posDiagonal.erase(pos);
+            negDiagonal.erase(neg);
+            board[row][col] = '.';
+        }
+    }
+    
+    vector<vector<string>> solveNQueens(int n) {
+        vector<string> board = vector<string>(n, string(n, '.'));
+        solve(0, n, board);
+        return sol;
+    }
+};

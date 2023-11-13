fa9fc5c6aaa743d52f9b6d18c4d17099cdb4d62c
Mohamed Motaz <53558209+Mohamed-Motaz@users.noreply.github.com>
2023-11-13 22:24:22 +0200

    Time: 19 ms (96.60%), Space: 18.2 MB (55.36%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.cpp
+++ 0143-reorder-list/0143-reorder-list.cpp
@@ -44,8 +44,69 @@ public:
         // }
     }
     void reorderList(ListNode* head) {
-        stackSol(head);
+        //stackSol(head);
+        reverseAndMerge(head);
     }
+    
+    void reverseAndMerge(ListNode* head){
+        //1 2 3 4 5
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+        
+        while (fast && fast->next){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        
+        ListNode* nextHalf = slow->next;
+        slow->next = NULL;
+        
+
+        
+        ListNode* l1 = head;
+        ListNode* l2 = reverse(nextHalf);
+
+        // while (l2){
+        //     cout << l2->val << " ";
+        //     l2 = l2->next;
+        // }
+        // cout << endl;
+     
+
+        merge(l1, l2);
+    }
+    
+    ListNode* reverse(ListNode* head) {
+        //123
+        ListNode* prev = NULL;
+
+        while (head){
+            ListNode* next = head->next;
+            head->next = prev;
+            prev = head;
+            head = next;
+        }
+        
+        return prev;
+    }
+    //1 2 3     4 5
+    void merge(ListNode* l1, ListNode* l2){
+       
+        while (l2){
+            ListNode* first = l1->next;
+            ListNode* second = l2->next;
+            
+            l1->next = l2;
+            l2->next = first;
+            
+            l1 = first;
+            l2 = second;
+        }
+        
+    }
+    
+    
 };
 
 // [1,2,3,4,5]

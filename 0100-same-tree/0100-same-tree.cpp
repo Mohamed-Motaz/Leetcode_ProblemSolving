2a79860f796e66ad296a9d988f1d8ebe2cef7f05
Mohamed Motaz <53558209+Mohamed-Motaz@users.noreply.github.com>
2023-11-14 23:15:38 +0200

    Time: 0 ms (100.00%), Space: 10.3 MB (59.88%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -14,13 +14,10 @@ public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
         if (!p && !q)
             return 1;
-        if (!p || !q) 
+        if ((p && !q) || (!p && q))
             return 0;
-        
-        if (p->val != q->val) 
+        if (p->val != q->val)
             return 0;
-        
-        return isSameTree(p->right, q->right) && isSameTree(p->left, q->left);
-         
+        return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
     }
 };

cb5ee26d977c8e5c161fb959b701e09efed99dbc
Mohamed Motaz <53558209+Mohamed-Motaz@users.noreply.github.com>
2023-11-18 21:16:06 +0200

    Time: 30 ms (67.28%), Space: 18.7 MB (75.18%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,36 +1,28 @@
 class Solution {
 public:
     vector<vector<int>> dp;
-    int work(string &s, string &t, int sI, int tI)
-    {
-        if (tI == t.size()) // tI has indeed reached the end
+    int solve(int i, int j, string &s, string &t){
+        if (j == t.size()){
             return 1;
-
-        if (sI == s.size()) // sI has reached the end, but without tI as well
+        }
+        if (i == s.size())
             return 0;
-
-        int &ans = dp[sI][tI];
+        
+        int &ans = dp[i][j];
         if (ans != -1)
             return ans;
-        ans = 0;
-        if (s[sI] != t[tI])
-        {
-            // forced to leaved
-            ans += work(s, t, sI + 1, tI);
-        }
-        else
-        {
-            ans += work(s, t, sI + 1, tI + 1) + // take
-                   work(s, t, sI + 1, tI);      // leave
-        }
+        
+        ans = solve(i + 1, j, s, t); //leave the current s char
+        
+        if (s[i] == t[j])
+            ans += solve(i + 1, j + 1, s, t);
+        
         return ans;
+        
     }
-    int numDistinct(string s, string t)
-    {
-        const int n = s.size() + 1, m = t.size() + 1;
-        dp.resize(n, vector<int>(m, -1));
-
-        return work(s, t, 0, 0);
+    
+    int numDistinct(string s, string t) {
+        dp.resize(s.size(), vector<int>(t.size(), -1));
+        return solve(0, 0, s, t);
     }
-
 };

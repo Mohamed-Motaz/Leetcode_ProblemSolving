6b0776983c8027241696cebd9dfff6af6f1ba7ce
Mohamed Motaz <53558209+Mohamed-Motaz@users.noreply.github.com>
2023-11-19 22:09:22 +0200

    Time: 30 ms (20.55%), Space: 13 MB (94.35%) - LeetHub

---
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    
+   
+    int findCheapestPrice(int n, vector<vector<int>> &flights, int src, int dst, int k)
+    {
+        // vector<vector<pair<int, int>>> adjList(n + 1);
+        // for (int i = 0; i < flights.size(); i++)
+        // {
+        //     adjList[flights[i][0]].push_back(make_pair(flights[i][1], flights[i][2]));
+        // }
+               
+        vector<int> dist(n, INT32_MAX);
+        dist[src] = 0;
+        for (int x = 0; x < k + 1; x++){
+            vector<int> copy = dist;
+            for (int i = 0; i < flights.size(); i++){
+                    int src = flights[i][0];
+                    int dst = flights[i][1];
+                    int cost = flights[i][2];
+                
+                    if (dist[src] == INT32_MAX)
+                        continue;
+                    if (dist[src] + cost < copy[dst])
+                        copy[dst] = dist[src] + cost;
+                    
+            }
+            dist = copy;
+        }
+        
+        return dist[dst] == INT32_MAX ? -1 : dist[dst];
+        
+    }
+};

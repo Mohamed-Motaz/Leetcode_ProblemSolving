7e42cb3ddcbeddc55623e398908f3743642ed28f
Mohamed Motaz <53558209+Mohamed-Motaz@users.noreply.github.com>
2023-12-01 23:53:35 +0200

    Time: 42 ms (90.57%), Space: 47.2 MB (23.78%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.cpp
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.cpp
@@ -1,39 +1,9 @@
 class TrieNode{
 public:
-    const int CAPACITY = 26;
     vector<TrieNode*> children;
-    int wordEnd;
-    
+    int endOfWord = 0;
     TrieNode(){
-        children.resize(CAPACITY, NULL);
-        wordEnd = 0;
-    }
-    
-    void addWord(TrieNode* root, string s){
-        for (char c: s){
-            TrieNode* child = root->children[c - 'a'];
-            if (!child)
-                child = new TrieNode();
-            root->children[c - 'a'] = child;
-            root = child;
-        }
-        root->wordEnd++;
-    }
-    
-    bool search(TrieNode* root, string s){
-        for (char c: s){
-            if (!root->children[c - 'a']) return 0;
-            root = root->children[c - 'a'];
-        }
-        return root->wordEnd > 0;
-    }
-    
-    bool prefix(TrieNode* root, string s){
-        for (char c: s){
-            if (!root->children[c - 'a']) return 0;
-            root = root->children[c - 'a'];
-        }
-        return 1;
+        children.resize(26);
     }
 };
 
@@ -45,15 +15,35 @@ public:
     }
     
     void insert(string word) {
-        root->addWord(root, word);
+        TrieNode* copy = root;
+        for (char c: word){
+            if (!copy->children[c - 'a'])
+                copy->children[c - 'a'] = new TrieNode();
+            copy = copy->children[c - 'a'];
+        }
+        copy->endOfWord++;
     }
     
     bool search(string word) {
-        return root->search(root, word);
+        TrieNode* copy = root;
+
+        for (char c: word){
+            if (!copy->children[c - 'a'])
+                return 0;
+            copy = copy->children[c - 'a'];
+        }
+        return copy->endOfWord;
     }
     
     bool startsWith(string prefix) {
-        return root->prefix(root, prefix);
+        TrieNode* copy = root;
+
+        for (char c: prefix){
+            if (!copy->children[c - 'a'])
+                return 0;
+            copy = copy->children[c - 'a'];
+        }
+        return 1;
     }
 };
 
@@ -64,3 +54,70 @@ public:
  * bool param_2 = obj->search(word);
  * bool param_3 = obj->startsWith(prefix);
  */
+
+// class TrieNode{
+// public:
+//     const int CAPACITY = 26;
+//     vector<TrieNode*> children;
+//     int wordEnd;
+    
+//     TrieNode(){
+//         children.resize(CAPACITY, NULL);
+//         wordEnd = 0;
+//     }
+    
+//     void addWord(TrieNode* root, string s){
+//         for (char c: s){
+//             TrieNode* child = root->children[c - 'a'];
+//             if (!child)
+//                 child = new TrieNode();
+//             root->children[c - 'a'] = child;
+//             root = child;
+//         }
+//         root->wordEnd++;
+//     }
+    
+//     bool search(TrieNode* root, string s){
+//         for (char c: s){
+//             if (!root->children[c - 'a']) return 0;
+//             root = root->children[c - 'a'];
+//         }
+//         return root->wordEnd > 0;
+//     }
+    
+//     bool prefix(TrieNode* root, string s){
+//         for (char c: s){
+//             if (!root->children[c - 'a']) return 0;
+//             root = root->children[c - 'a'];
+//         }
+//         return 1;
+//     }
+// };
+
+// class Trie {
+// public:
+//     TrieNode* root;
+//     Trie() {
+//         root = new TrieNode();
+//     }
+    
+//     void insert(string word) {
+//         root->addWord(root, word);
+//     }
+    
+//     bool search(string word) {
+//         return root->search(root, word);
+//     }
+    
+//     bool startsWith(string prefix) {
+//         return root->prefix(root, prefix);
+//     }
+// };
+
+// /**
+//  * Your Trie object will be instantiated and called as such:
+//  * Trie* obj = new Trie();
+//  * obj->insert(word);
+//  * bool param_2 = obj->search(word);
+//  * bool param_3 = obj->startsWith(prefix);
+//  */

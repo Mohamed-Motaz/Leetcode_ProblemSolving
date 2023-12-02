e9edde4105bfb93cff678502be7121cfe1b9c11c
Mohamed Motaz <53558209+Mohamed-Motaz@users.noreply.github.com>
2023-12-02 14:17:09 +0200

    Time: 0 ms (100.00%), Space: 11.2 MB (89.28%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.cpp
@@ -1,23 +1,33 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        
-        int prev = INT32_MAX;
         int s = 0;
-        bool oops = 0;
         for (int i = 0; i < nums.size(); i++){
-            if (nums[i] == prev){
-                if (!oops){
-                    nums[s++] = nums[i];
-                    oops = 1;
-                }
-            }else{
-                nums[s++] = nums[i];
-                oops = 0;
+            if (i < 2 || nums[s - 2] != nums[i]){
+                nums[s] = nums[i];
+                s++;
             }
-            prev = nums[i];
         }
-        
         return s;
     }
+//     int removeDuplicates(vector<int>& nums) {
+        
+//         int prev = INT32_MAX;
+//         int s = 0;
+//         bool oops = 0;
+//         for (int i = 0; i < nums.size(); i++){
+//             if (nums[i] == prev){
+//                 if (!oops){
+//                     nums[s++] = nums[i];
+//                     oops = 1;
+//                 }
+//             }else{
+//                 nums[s++] = nums[i];
+//                 oops = 0;
+//             }
+//             prev = nums[i];
+//         }
+        
+//         return s;
+//     }
 };

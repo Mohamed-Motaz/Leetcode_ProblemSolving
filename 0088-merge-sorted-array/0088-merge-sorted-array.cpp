8bdb3bfe031417e98c8c3174ea87e57832aa3f9e
Mohamed Motaz <53558209+Mohamed-Motaz@users.noreply.github.com>
2023-12-02 10:36:11 +0200

    Time: 5 ms (29.71%), Space: 9.5 MB (39.88%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -4,37 +4,23 @@ public:
         //1 3 5 7 0 0 0 0
         //2 4 6 8
         
-        //edge case
-        if (!m){
-            while (n--){
-                nums1[n] = nums2[n];
-            }
-            return;
-        }
         
-        
-        int endVec = nums1.size() - 1;
+        //nums2 should be merged into nums1
         int end1 = m - 1;
         int end2 = n - 1;
-        
-        while (end1 >= 0 && end2 >= 0){
-            if (nums1[end1] >= nums2[end2]){
-                nums1[endVec] = nums1[end1];
-                endVec--;
-                end1--;
-            }else{
-                nums1[endVec] = nums2[end2];
-                endVec--;
-                end2--;
-            }
-        }
+        int endVec = n + m - 1;
         
         while (end2 >= 0){
-            nums1[endVec] = nums2[end2];
-            end2--;
-            endVec--;
+            if (end1 >= 0 && nums1[end1] >= nums2[end2]){
+                nums1[endVec] = nums1[end1];
+                end1--;
+                endVec--;
+            }else{
+                nums1[endVec] = nums2[end2];
+                end2--;
+                endVec--;
+            }
         }
-        
 
     }
 };

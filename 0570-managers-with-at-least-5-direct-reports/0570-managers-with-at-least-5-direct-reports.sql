81bef2bfe2494e4fcf6a2590d2e7f7d2b4a5be1a
Mohamed Motaz <53558209+Mohamed-Motaz@users.noreply.github.com>
2024-01-14 00:25:54 +0200

    Time: 572 ms (68.52%), Space: 0B (100.00%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -11,10 +11,18 @@
 #     having count(*) >= 5
 # );
 
+
 select e2.name
-from employee as e1
-inner join employee as e2
-on e1.managerId = e2.id
-where e1.managerId IS NOT NULL
+from employee as e2
+inner join employee as e1
+on e2.id = e1.managerId
 group by e1.managerId
-having count(*) >= 5
+having count(*) >= 5;
+
+# select e2.name
+# from employee as e1
+# inner join employee as e2
+# on e1.managerId = e2.id
+# where e1.managerId IS NOT NULL
+# group by e1.managerId
+# having count(*) >= 5

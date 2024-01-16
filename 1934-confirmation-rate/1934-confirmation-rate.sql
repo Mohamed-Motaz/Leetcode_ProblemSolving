db4131cfb98068c28e9937bc98944d9b90fecfc7
Mohamed Motaz <53558209+Mohamed-Motaz@users.noreply.github.com>
2024-01-17 00:48:12 +0200

    Time: 1257 ms (50.31%), Space: 0B (100.00%) - LeetHub

--- 1934-confirmation-rate/1934-confirmation-rate.sql
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -1,23 +1,32 @@
 # Write your MySQL query statement below
 
 
-select signups.user_id, 
-ROUND(
-    IFNULL(confirmed.count, 0)/ IFNULL(total.count, 1) ,2)
-    as confirmation_rate
-from signups
-left join (
-    select user_id, count(*) as count
-    from confirmations
-    group by user_id, action
-    having action = "confirmed"
-    ) as confirmed
-on signups.user_id = confirmed.user_id
-left join (
-    select user_id, count(*) as count
-    from confirmations
-    group by user_id
-    ) as total
-on signups.user_id = total.user_id;
+# select signups.user_id, 
+# ROUND(
+#     IFNULL(confirmed.count, 0)/ IFNULL(total.count, 1) ,2)
+#     as confirmation_rate
+# from signups
+# left join (
+#     select user_id, count(*) as count
+#     from confirmations
+#     group by user_id, action
+#     having action = "confirmed"
+#     ) as confirmed
+# on signups.user_id = confirmed.user_id
+# left join (
+#     select user_id, count(*) as count
+#     from confirmations
+#     group by user_id
+#     ) as total
+# on signups.user_id = total.user_id;
+
+
+SELECT signups.user_id, ROUND(AVG(IF(c.action = "confirmed", 1, 0)) , 2) as confirmation_rate
+FROM signups
+LEFT JOIN confirmations as c
+ON c.user_id = signups.user_id
+GROUP BY user_id
+
+
 
 

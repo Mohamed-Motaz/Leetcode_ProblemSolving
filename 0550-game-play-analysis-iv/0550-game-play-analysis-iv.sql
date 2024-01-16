4c267e9c773d53035933557a42d41b8c2f1418dc
Mohamed Motaz <53558209+Mohamed-Motaz@users.noreply.github.com>
2024-01-16 20:13:13 +0200

    Time: 3033 ms (5.01%), Space: 0B (100.00%) - LeetHub

--- 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
+++ 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
@@ -1,32 +1,51 @@
 # Write your MySQL query statement below
 
 
-WITH mn as
-(
-    SELECT player_id, MIN(event_date) as first_day
-    FROM activity
-    GROUP BY player_id
-), 
-cnt as
-(
-    SELECT COUNT(*) as total_logged_twice
-    FROM activity as a
-    INNER JOIN mn
-    ON a.player_id = mn.player_id
-    AND DATE_SUB(event_date, INTERVAL 1 DAY) = mn.first_day
-)
+#####################CTE#######################
+# WITH mn as
+# (
+#     SELECT player_id, MIN(event_date) as first_day
+#     FROM activity
+#     GROUP BY player_id
+# ), 
+# cnt as
+# (
+#     SELECT COUNT(*) as total_logged_twice
+#     FROM activity as a
+#     INNER JOIN mn
+#     ON a.player_id = mn.player_id
+#     AND DATE_SUB(event_date, INTERVAL 1 DAY) = mn.first_day
+# )
+
+# # SELECT ROUND(
+# #             CAST(cnt.total_logged_twice as DECIMAL) / 
+# #             CAST(COUNT(distinct activity.player_id) as DECIMAL), 
+# #     2) as fraction
+# # FROM cnt
+# # INNER JOIN activity;
 
 # SELECT ROUND(
-#             CAST(cnt.total_logged_twice as DECIMAL) / 
-#             CAST(COUNT(distinct activity.player_id) as DECIMAL), 
-#     2) as fraction
-# FROM cnt
-# INNER JOIN activity;
+#     (SELECT total_logged_twice from cnt) /
+#     (SELECT count(player_id) from mn),
+#     2) as fraction;
 
-SELECT ROUND(
-    (SELECT total_logged_twice from cnt) /
-    (SELECT count(player_id) from mn),
-    2) as fraction;
+
+
+#NORMAL SOL
+
+SELECT 
+    ROUND(
+        COUNT(*) / 
+        (SELECT COUNT(Distinct player_id) from activity), 
+    2) as fraction
+FROM activity
+WHERE (player_id, DATE_SUB(event_date, INTERVAL 1 DAY)) in
+(
+    SELECT player_id, min(event_date) as first_login
+    FROM activity
+    GROUP BY player_id
+)
+
 
 
 

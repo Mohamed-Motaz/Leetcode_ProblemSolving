7538f99435cf89586ed7146e0523dd89b63e635d
Mohamed Motaz <53558209+Mohamed-Motaz@users.noreply.github.com>
2024-01-17 19:09:32 +0200

    Time: 3289 ms (5.01%), Space: 0B (100.00%) - LeetHub

--- 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
+++ 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
@@ -2,28 +2,30 @@
 
 #great question
 
-#first case, get all that have a change date before 8-16
-SELECT product_id, 10 as price
-FROM products
-GROUP BY product_id
-HAVING MIN(change_date) > "2019-08-16"
+# #first sol ######################################################################################
+# #first case, get all that have a change date before 8-16
+# SELECT product_id, 10 as price
+# FROM products
+# GROUP BY product_id
+# HAVING MIN(change_date) > "2019-08-16"
 
-UNION ALL
+# UNION ALL
 
-#second case, get all that have a change date before 8-16
-SELECT product_id, new_price
-    FROM products
-    WHERE (product_id, change_date) 
-    IN
-    (
-        SELECT product_id, MAX(change_date)
-        FROM products
-        WHERE change_date <= "2019-08-16"
-        GROUP BY product_id
-    ) 
+# #second case, get all that have a change date before 8-16
+# SELECT product_id, new_price
+#     FROM products
+#     WHERE (product_id, change_date) 
+#     IN
+#     (
+#         SELECT product_id, MAX(change_date)
+#         FROM products
+#         WHERE change_date <= "2019-08-16"
+#         GROUP BY product_id
+#     ) 
 
 
 
+#second sol ######################################################################################
 
 # SELECT products.product_id as product_id, IFNULL(p.new_price, 10) as price
 # FROM products
@@ -41,3 +43,22 @@ SELECT product_id, new_price
 #     ) 
 # ) as p on products.product_id = p.product_id
 # GROUP BY product_id
+
+#third sol ######################################################################################
+
+SELECT products.product_id as product_id, IFNULL(p.new_price, 10) as price
+FROM products
+LEFT JOIN 
+(
+    SELECT products.product_id, products.new_price
+    FROM products
+    INNER JOIN
+    (
+        SELECT product_id, MAX(change_date) as change_date
+        FROM products
+        WHERE change_date <= "2019-08-16"
+        GROUP BY product_id
+    ) as pp
+    ON products.product_id = pp.product_id AND products.change_date = pp.change_date
+) as p on products.product_id = p.product_id
+GROUP BY product_id

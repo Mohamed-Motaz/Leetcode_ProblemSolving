10738c16c1a8b4ade75a6c085c4f5a8289e2aeb2
Mohamed Motaz <53558209+Mohamed-Motaz@users.noreply.github.com>
2024-01-17 19:10:02 +0200

    Time: 762 ms (81.91%), Space: 0B (100.00%) - LeetHub

--- 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
+++ 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
@@ -27,38 +27,38 @@
 
 #second sol ######################################################################################
 
-# SELECT products.product_id as product_id, IFNULL(p.new_price, 10) as price
-# FROM products
-# LEFT JOIN 
-# (
-#     SELECT product_id, new_price
-#     FROM products
-#     WHERE (product_id, change_date) 
-#     IN
-#     (
-#         SELECT product_id, MAX(change_date)
-#         FROM products
-#         WHERE change_date <= "2019-08-16"
-#         GROUP BY product_id
-#     ) 
-# ) as p on products.product_id = p.product_id
-# GROUP BY product_id
-
-#third sol ######################################################################################
-
 SELECT products.product_id as product_id, IFNULL(p.new_price, 10) as price
 FROM products
 LEFT JOIN 
 (
-    SELECT products.product_id, products.new_price
+    SELECT product_id, new_price
     FROM products
-    INNER JOIN
+    WHERE (product_id, change_date) 
+    IN
     (
-        SELECT product_id, MAX(change_date) as change_date
+        SELECT product_id, MAX(change_date)
         FROM products
         WHERE change_date <= "2019-08-16"
         GROUP BY product_id
-    ) as pp
-    ON products.product_id = pp.product_id AND products.change_date = pp.change_date
+    ) 
 ) as p on products.product_id = p.product_id
 GROUP BY product_id
+
+#third sol ######################################################################################
+
+# SELECT products.product_id as product_id, IFNULL(p.new_price, 10) as price
+# FROM products
+# LEFT JOIN 
+# (
+#     SELECT products.product_id, products.new_price
+#     FROM products
+#     INNER JOIN
+#     (
+#         SELECT product_id, MAX(change_date) as change_date
+#         FROM products
+#         WHERE change_date <= "2019-08-16"
+#         GROUP BY product_id
+#     ) as pp
+#     ON products.product_id = pp.product_id AND products.change_date = pp.change_date
+# ) as p on products.product_id = p.product_id
+# GROUP BY product_id

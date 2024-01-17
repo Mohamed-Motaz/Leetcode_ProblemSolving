a6eb0877b94b430694245e051d787d7f915a54c1
Mohamed Motaz <53558209+Mohamed-Motaz@users.noreply.github.com>
2024-01-17 19:06:13 +0200

    Time: 694 ms (98.35%), Space: 0B (100.00%) - LeetHub

--- 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
+++ 1164-product-price-at-a-given-date/1164-product-price-at-a-given-date.sql
@@ -1,10 +1,17 @@
 # Write your MySQL query statement below
 
-SELECT products.product_id as product_id, IFNULL(p.new_price, 10) as price
+#great question
+
+#first case, get all that have a change date before 8-16
+SELECT product_id, 10 as price
 FROM products
-LEFT JOIN 
-(
-    SELECT product_id, new_price
+GROUP BY product_id
+HAVING MIN(change_date) > "2019-08-16"
+
+UNION ALL
+
+#second case, get all that have a change date before 8-16
+SELECT product_id, new_price
     FROM products
     WHERE (product_id, change_date) 
     IN
@@ -14,5 +21,23 @@ LEFT JOIN
         WHERE change_date <= "2019-08-16"
         GROUP BY product_id
     ) 
-) as p on products.product_id = p.product_id
-GROUP BY product_id
+
+
+
+
+# SELECT products.product_id as product_id, IFNULL(p.new_price, 10) as price
+# FROM products
+# LEFT JOIN 
+# (
+#     SELECT product_id, new_price
+#     FROM products
+#     WHERE (product_id, change_date) 
+#     IN
+#     (
+#         SELECT product_id, MAX(change_date)
+#         FROM products
+#         WHERE change_date <= "2019-08-16"
+#         GROUP BY product_id
+#     ) 
+# ) as p on products.product_id = p.product_id
+# GROUP BY product_id

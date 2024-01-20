e44774964b9a59184573bee19a9cc0f2dbe0ff81
Mohamed Motaz <53558209+Mohamed-Motaz@users.noreply.github.com>
2024-01-20 19:07:09 +0200

    Time: 1934 ms (42.34%), Space: 0B (100.00%) - LeetHub

--- 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
+++ 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
@@ -2,17 +2,69 @@
 
 
 
-
-
-
-
-SELECT product_id, year as first_year, quantity, price
-FROM sales
-WHERE 
-    (product_id, year) 
-IN
+WITH first_year as
 (
     SELECT product_id, MIN(year) as first_year
-    FROM SALES
+    FROM Sales
     GROUP BY product_id
 )
+SELECT s.product_id, fy.first_year, s.quantity, s.price
+FROM Sales as s
+INNER JOIN first_year as fy
+ON 
+    s.product_id = fy.product_id 
+    AND
+    s.year = fy.first_year;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# SELECT product_id, year as first_year, quantity, price
+# FROM sales
+# WHERE 
+#     (product_id, year) 
+# IN
+# (
+#     SELECT product_id, MIN(year) as first_year
+#     FROM SALES
+#     GROUP BY product_id
+# )

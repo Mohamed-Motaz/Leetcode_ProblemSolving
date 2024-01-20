be93534fc4821f12637a06c3e2a7064c0065cd7b
Mohamed Motaz <53558209+Mohamed-Motaz@users.noreply.github.com>
2024-01-20 15:13:25 +0200

    Time: 522 ms (39.76%), Space: 0B (100.00%) - LeetHub

--- 0176-second-highest-salary/0176-second-highest-salary.sql
+++ 0176-second-highest-salary/0176-second-highest-salary.sql
@@ -1,11 +1,24 @@
 # Write your MySQL query statement below
-WITH res as (
-    SELECT distinct salary
-    FROM employee
-    ORDER BY salary DESC
-    LIMIT 1
-    OFFSET 1
-)
-SELECT (CASE WHEN COUNT(*) = 0 THEN NULL
-        ELSE salary END) as SecondHighestSalary
-FROM res
+
+SELECT (
+    CASE 
+    WHEN COUNT(distinct salary)=1 THEN NULL
+    ELSE (SELECT salary FROM Employee GROUP BY salary ORDER BY salary DESC LIMIT 1 OFFSET 1 )
+    END) 
+    as SecondHighestSalary
+FROM employee
+
+
+
+# MY NEW SOL
+# WITH res as (
+#     SELECT distinct salary
+#     FROM employee
+#     ORDER BY salary DESC
+#     LIMIT 1
+#     OFFSET 1
+# )
+# SELECT (CASE WHEN COUNT(*) = 0 THEN NULL
+#         ELSE salary END) as SecondHighestSalary
+# FROM res
+

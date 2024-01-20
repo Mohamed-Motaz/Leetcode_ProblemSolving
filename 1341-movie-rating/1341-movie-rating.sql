acb81576fbba6f7ed1f30ac21878df25785e229e
Mohamed Motaz <53558209+Mohamed-Motaz@users.noreply.github.com>
2024-01-20 16:20:22 +0200

    Time: 3861 ms (5.01%), Space: 0B (100.00%) - LeetHub

--- 1341-movie-rating/1341-movie-rating.sql
+++ 1341-movie-rating/1341-movie-rating.sql
@@ -1,25 +1,51 @@
 # Write your MySQL query statement below
 
+# (
+# SELECT users.name as results
+# FROM MovieRating as m
+# INNER JOIN users
+# ON users.user_id = m.user_id
+# GROUP BY (m.user_id)
+# ORDER BY COUNT(*) DESC, users.name
+# LIMIT 1
+# )
+# UNION ALL
+# (
+# SELECT movies.title as results
+# FROM MovieRating as m
+# INNER JOIN movies
+# ON movies.movie_id = m.movie_id
+# WHERE SUBSTR(created_at, 1, 7) = "2020-02"
+# GROUP BY (m.movie_id)
+# ORDER BY AVG(m.rating) DESC, movies.title
+# LIMIT 1
+# )
+
+
+
+# NEW
 (
-SELECT users.name as results
+SELECT u.name as results
 FROM MovieRating as m
-INNER JOIN users
-ON users.user_id = m.user_id
-GROUP BY (m.user_id)
-ORDER BY COUNT(*) DESC, users.name
+INNER JOIN Users as u
+ON m.user_id = u.user_id
+GROUP BY m.user_id
+ORDER BY COUNT(*) DESC, u.name
 LIMIT 1
 )
-UNION ALL
+UNION ALL #must be union all, not union, because if user has same name as user, it won't be returned
+
 (
-SELECT movies.title as results
+SELECT u.title as results
 FROM MovieRating as m
-INNER JOIN movies
-ON movies.movie_id = m.movie_id
+INNER JOIN Movies as u
+ON m.movie_id = u.movie_id
 WHERE SUBSTR(created_at, 1, 7) = "2020-02"
-GROUP BY (m.movie_id)
-ORDER BY AVG(m.rating) DESC, movies.title
+GROUP BY m.movie_id
+ORDER BY AVG(rating) DESC, u.title
 LIMIT 1
 )
+
 
 
 # (

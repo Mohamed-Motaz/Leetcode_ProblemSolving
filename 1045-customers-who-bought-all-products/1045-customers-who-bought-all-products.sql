ba5312efafe722e5164e3574d941f98ea1f89bd3
Mohamed Motaz <53558209+Mohamed-Motaz@users.noreply.github.com>
2024-01-20 18:54:59 +0200

    Time: 1194 ms (35.21%), Space: 0B (100.00%) - LeetHub

--- 1045-customers-who-bought-all-products/1045-customers-who-bought-all-products.sql
+++ 1045-customers-who-bought-all-products/1045-customers-who-bought-all-products.sql
@@ -1,34 +1,45 @@
 # Write your MySQL query statement below
 
 
+
 SELECT customer_id
-FROM customer
+FROM Customer
 GROUP BY customer_id
-HAVING COUNT(distinct product_key) = (SELECT COUNT(*) from product)
+HAVING(COUNT(distinct product_key)) = (
+    SELECT COUNT(*)
+    FROM Product
+)
 
 
-
-# #clean up
-# WITH clean_up as
-# (
-#     SELECT customer_id, product_key
-#     FROM customer
-#     GROUP BY customer_id, product_key
-# ),
-# counts as 
-# (
-#     SELECT customer_id, COUNT(*) as prods
-#     FROM clean_up
-#     GROUP BY customer_id
-# ),
-# tot_prods as
-# (
-#     SELECT COUNT(*) as cnt
-#     FROM product
-# )
 
 # SELECT customer_id
-# FROM counts
-# JOIN tot_prods
-# WHERE prods = tot_prods.cnt;
+# FROM customer
+# GROUP BY customer_id
+# HAVING COUNT(distinct product_key) = (SELECT COUNT(*) from product)
+
+
+
+# # #clean up
+# # WITH clean_up as
+# # (
+# #     SELECT customer_id, product_key
+# #     FROM customer
+# #     GROUP BY customer_id, product_key
+# # ),
+# # counts as 
+# # (
+# #     SELECT customer_id, COUNT(*) as prods
+# #     FROM clean_up
+# #     GROUP BY customer_id
+# # ),
+# # tot_prods as
+# # (
+# #     SELECT COUNT(*) as cnt
+# #     FROM product
+# # )
+
+# # SELECT customer_id
+# # FROM counts
+# # JOIN tot_prods
+# # WHERE prods = tot_prods.cnt;
 

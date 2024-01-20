5615ecccd0b61ffc870a72e3e02798d205b0f8b3
Mohamed Motaz <53558209+Mohamed-Motaz@users.noreply.github.com>
2024-01-20 15:32:21 +0200

    Time: 1940 ms (39.93%), Space: 0B (100.00%) - LeetHub

--- 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
@@ -1,14 +1,13 @@
 # Write your MySQL query statement below
 
-SELECT d.name as Department, e.name as Employee, e.salary as Salary
-FROM EMPLOYEE as e
-INNER JOIN DEPARTMENT as d
-ON e.departmentId = d.id
-WHERE
-#correlated subquery
-(
+
+SELECT d.name as Department, e1.name as Employee, e1.salary
+FROM employee e1
+INNER JOIN department as d
+ON e1.departmentId = d.id
+WHERE (
     SELECT COUNT(distinct salary)
-    FROM EMPLOYEE
-    WHERE salary > e.salary AND departmentId = e.departmentId
-    LIMIT 3
-) < 3
+    FROM employee e2
+    WHERE e2.departmentId = e1.departmentId
+    AND e2.salary > e1.salary
+) <= 2
